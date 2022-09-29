05e87fe0315798e04f906f6890521ccc2b310c77
yourkeksuk <igor-18css@mail.ru>
2022-09-29 20:03:23 +0300

    Initial commit

--- src/test/java/NetologyTest.java
+++ src/test/java/NetologyTest.java
@@ -14,6 +14,6 @@ public class NetologyTest {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some value"));
+                .body("[0].data", equalTo("some value"));
     }
 }
